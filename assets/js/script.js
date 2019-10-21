54f96e9fba425f426eae27a2646fa0b3a5dddf5f
Tammer Galal <tammergalal20@gmail.com>
2019-10-21 16:55:29 -0400

    added some basic functionality to the purchase page

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+const purchaseBtn = document.getElementById("purchaseBtn");
+const modalEl = document.querySelector(".modal-content");
+const purchaseEmail = document.getElementById("purchaseEmail");
+
+
+function purchaseTicket () {
+  modalEl.textContent = `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
+  console.log("purchased a ticket");
+}
+purchaseBtn.addEventListener("click", purchaseTicket);
+
+}
