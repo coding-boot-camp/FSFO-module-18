43eeda4dc9e4627fe927a4c5f331ee008961a2a9
Alec Branaa <alec@ossenx.com>
2019-10-24 12:04:31 -0700

    Fix bugs on ticket page

--- assets/js/tickets.js
+++ assets/js/tickets.js
@@ -1,16 +1,15 @@
-import "bootstrap/js/dist/modal";
+require("bootstrap/js/dist/modal");
 
 $(document).ready(function() {
-	if(window.location.href.indexOf("ticket") > -1) {
-		const purchaseBtn = document.getElementById("purchaseBtn");
-		const modalEl = document.querySelector(".modal-content");
-		const purchaseEmail = document.getElementById("purchaseEmail");
+  const purchaseBtn = document.getElementById("purchaseBtn");
+  const modalEl = document.querySelector(".modal-content");
+  const purchaseEmail = document.getElementById("purchaseEmail");
 
 
-		function purchaseTicket () {
-			modalEl.textContent = `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
-			console.log("purchased a ticket");
-		}
-		purchaseBtn.addEventListener("click", purchaseTicket);
-	}
+  function purchaseTicket () {
+    modalEl.textContent = `Thanks for requesting a ticket purchase! We will send an email to ${purchaseEmail.value} to complete the order form!`
+    console.log("purchased a ticket");
+  }
+  purchaseBtn.addEventListener("click", purchaseTicket);
+
 });
