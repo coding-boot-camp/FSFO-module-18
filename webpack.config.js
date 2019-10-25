66cbe649ff0e38a11bd85797742146f9722a30b2
Alec Branaa <alec@ossenx.com>
2019-10-25 10:26:44 -0700

    start url to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -57,8 +57,7 @@ const config = {
       description: "An app that allows you to view upcoming food events.",
       background_color: "#01579b",
       theme_color: "#ffffff",
-      start_url: "./",
-      scope: ".",
+      start_url: "/index.html",
       icons: [{
         src: path.resolve("assets/img/icons/icon-512x512.png"),
         sizes: [96, 128, 192, 256, 384, 512],
